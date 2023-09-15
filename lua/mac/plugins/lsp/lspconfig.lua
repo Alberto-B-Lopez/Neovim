2dee7766632b33ad3321e4fde1e308db82dae17f
Alberto <Mralbertoblopez@gmail.com>
2023-09-15 19:50:56 -0400

    1.01 LSP updates

--- lua/mac/plugins/lsp/lspconfig.lua
+++ lua/mac/plugins/lsp/lspconfig.lua
@@ -20,7 +20,7 @@ return {
 
       -- set keybinds
       opts.desc = "Format"
-      keymap.set("n", "<leader>lf", vim.lsp.buf.format, opts) -- show definition, references
+      keymap.set("n", "<leader>lf", vim.lsp.buf.format, opts) --  auto formats
       
       opts.desc = "Show LSP references"
       keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
