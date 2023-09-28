aad429d64eaa34f00889a34007b143f39ec65dc8
Alberto <Mralbertoblopez@gmail.com>
2023-09-28 15:07:57 -0400

    Added new features Git Worktrees and Twilight/Zen

--- lua/mac/plugins/twilight.lua
+++ lua/mac/plugins/twilight.lua
@@ -1,3 +1,10 @@
 return {
   "folke/twilight.nvim",
+  config = function ()
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>tw", "<cmd>Twilight<CR>", { desc = "Activated and Deactivated Twilight"})
+    
+  end,
 }
