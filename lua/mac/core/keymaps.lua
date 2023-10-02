e9013ac83f3e5c6f604cb9be476e815d324666df
Alberto <Mralbertoblopez@gmail.com>
2023-10-02 12:29:28 -0400

    Added new linting and formatting

--- lua/mac/core/keymaps.lua
+++ lua/mac/core/keymaps.lua
@@ -1,8 +1,9 @@
 vim.g.mapleader = " "
-vim.api.nvim_set_keymap('n', '<leader>r', [[:!python3 %<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>r", [[:!python3 %<CR>]], { noremap = true, silent = true })
 
 local keymap = vim.keymap -- for conciseness
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
+keymap.set("i", "<C-f>", "<Right>", { desc = "Moves right in insert mode" })
+keymap.set("i", "<C-b>", "<Left>", { desc = "Moves left in insert mode" })
--- lua/mac/plugins/noice.lua
+++ lua/mac/plugins/noice.lua
@@ -1,28 +1,29 @@
-return { "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-    presets = {
-      long_message_to_split = true,
-      bottom_search = true,
-      command_palette = true,
-    },
-    messages = {
-      enabled = false,
-    },
-    popupmenu = {
-      enabled = false,
-    },
-    notify = {
-      enabled = false,
-    },
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-  }
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- add any options here
+		presets = {
+			long_message_to_split = true,
+			bottom_search = true,
+			command_palette = true,
+		},
+		messages = {
+			enabled = false,
+		},
+		popupmenu = {
+			enabled = true,
+		},
+		notify = {
+			enabled = false,
+		},
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
 }
