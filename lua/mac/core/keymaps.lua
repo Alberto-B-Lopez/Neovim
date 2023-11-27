23eb5c454cc06a9a29ad9acc88091b064adb891f
Mac Lopez <mac@imac.mynetworksettings.com>
2023-11-26 21:04:27 -0500

    Added Select all keymap

--- lua/mac/core/keymaps.lua
+++ lua/mac/core/keymaps.lua
@@ -4,6 +4,7 @@ vim.api.nvim_set_keymap("n", "<leader>r", [[:!python3 %<CR>]], { noremap = true,
 local keymap = vim.keymap -- for conciseness
 
 -- clear search highlights
+keymap.set("n", "<leader>a", "ggVG", { desc = "Select all" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("i", "<C-f>", "<Right>", { desc = "Moves right in insert mode" })
 keymap.set("i", "<C-b>", "<Left>", { desc = "Moves left in insert mode" })
